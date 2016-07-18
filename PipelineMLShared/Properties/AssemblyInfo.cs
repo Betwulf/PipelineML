2609790d85b607797cfcb0b02f8c293b140c3a5a
Betwulf <betwulf@hotmail.com>
2016-07-18 15:57:29 -0400

    Created Shared Library with Interfaces and base classes for those who want to write their own Data Transforms and whatnot

--- PipelineMLShared/Properties/AssemblyInfo.cs
+++ PipelineMLShared/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PipelineMLInterfaces")]
+[assembly: AssemblyTitle("PipelineMLShared")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PipelineMLInterfaces")]
+[assembly: AssemblyProduct("PipelineMLShared")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4423e3f4-37a8-4532-abdb-9a62c030f70c")]
+[assembly: Guid("7124dff2-87ba-486d-bc09-53e97e7bad9a")]
 
 // Version information for an assembly consists of the following four values:
 //
