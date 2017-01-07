c91019956d93435ce51751772d165cc3292097f9
Betwulf <betwulf@hotmail.com>
2017-01-07 15:20:53 -0500

    Added PipelineProject class.... To be used in the website.

---
+++ PipelineMLCore/Pipeline/PipelineProject.cs
@@ -0,0 +1,37 @@
+﻿using Ninject;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PipelineMLCore.Pipeline
+{
+    public class PipelineProject
+    {
+        private IKernel _kernel;
+
+        public PipelineProject()
+        {
+
+        }
+
+        public void Configure(IKernel kernel)
+        {
+            _kernel = kernel;
+        }
+
+        [Required, MaxLength(40)]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        [Required]
+        public Guid Id { get; set; }
+
+        public PipelineDefinition Definition { get; set; }
+
+        public List<PipelineResults> Results { get; set; }
+    }
+}
